6d7523c4854fe4cf9e61f65f4ceccd4a4a41bf3a
Vishwashikha Patel <99236027+Vishwashikha@users.noreply.github.com>
2026-04-18 15:19:00 +0530

    Added Day 1 python practice programs

---
+++ day1_condition.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+if num1>num2:
+    print("Greatest no:", num1)
+else:
+    print("Greatest no:",num2)
---
+++ day1_print.py
@@ -0,0 +1,6 @@
+name = "Vishwashikha Patel"
+age = 23
+city = " Patna"
+print(name)
+print(age)
+print(city)
